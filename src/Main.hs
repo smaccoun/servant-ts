c5c54e3fcd977a935206a66317b5d2d1a4c94a54
Steven MacCoun <theraccoun@gmail.com>
2018-09-21 16:16:06 -0400

    Basic api example for development

--- src/Main.hs
+++ src/Main.hs
@@ -1,16 +1,28 @@
+{-# LANGUAGE DataKinds             #-}
+{-# LANGUAGE DeriveAnyClass        #-}
+{-# LANGUAGE DeriveGeneric         #-}
 {-# LANGUAGE FlexibleContexts      #-}
 {-# LANGUAGE FlexibleInstances     #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings     #-}
 {-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE StandaloneDeriving    #-}
 {-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE TypeOperators         #-}
 
 module Main where
 
 import           Data.Maybe
-import           Data.Proxy       (Proxy (Proxy))
+import           Data.Proxy          (Proxy (Proxy))
+import           Data.Text
 import           GenericMappings
-import           Servant.Foreign  (Foreign, GenerateList, HasForeign,
-                                   HasForeignType, Req, listFromAPI, typeFor)
+import           GenericMappings
+import           GHC.Generics
+import           Servant.API
+import           Servant.Foreign     (Foreign, GenerateList, HasForeign,
+                                      HasForeignType, Req, listFromAPI, typeFor,
+                                      _reqReturnType)
+import           Typescript.Generate
 import           Typescript.Types
 
 
@@ -25,8 +37,18 @@ servantToTS ::
          => Proxy api
          -> [Req TSType]
 servantToTS =
-    listFromAPI (Proxy :: Proxy LangTypescript) (Proxy :: Proxy TSType) 
+    listFromAPI (Proxy :: Proxy LangTypescript) (Proxy :: Proxy TSType)
+
+data User = User
+    {name    :: Text
+    ,age     :: Int
+    ,isAdmin :: Bool
+    } deriving (Generic, TypescriptType)
+
+type API = "users" :> Get '[JSON] User
 
 main :: IO ()
 main = do
-  putStrLn "Init"
+  let (asTS :: [Req TSType]) = servantToTS (Proxy :: Proxy API)
+      allTypes = (fromMaybe (TSPrimitiveType TSString) <$> _reqReturnType) <$> asTS
+  putStrLn $ show $ toTypescript <$> allTypes
