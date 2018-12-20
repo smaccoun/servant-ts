cc4dbff7e8be97c00f16a06e36ed84bb961c80f6
Steven MacCoun <theraccoun@gmail.com>
2018-12-19 18:44:32 -0700

    Added bulma

--- example/frontend/src/index.tsx
+++ example/frontend/src/index.tsx
@@ -2,7 +2,5 @@ import * as React from 'elm-ts/lib/React'
 import { render } from 'react-dom'
 import * as component from './playground'
 
-console.log("HI THERE!")
-
 const main = React.program(component.init, component.update, component.view)
 React.run(main, dom => render(dom, document.getElementById('app')!))
--- example/frontend/src/playground.tsx
+++ example/frontend/src/playground.tsx
@@ -20,47 +20,68 @@ export function update(msg: Msg, model: Model): [Model, Cmd<Msg>] {
 
 export function view(model: Model): Html<Msg> {
   return dispatch => (
-    <div style={{display: 'flex', width: '100%'}}>
-      <div style={{display: 'flex', flexDirection: 'column', border: '1px solid black'}}>
-        <h1>API</h1>
-        <SyntaxHighlighter language='haskell'>
-          {`
-            type SimpleAPI =
-                     "user" :> Get '[JSON] [User]
-                :<|> "user" :> Capture "userId" Int :> Get '[JSON] User
-          `}
-        </SyntaxHighlighter>
-      </div>
-      <div style={{display: 'flex', flexDirection: 'column', width: '100%'}}>
-        <div style={{display: 'flex', flexDirection: 'column', width: '100%', alignItems: 'center'}}>
-          <h4>Server/types.tsx</h4>
-          <SyntaxHighlighter language='typescript'>
-            {`
-              interface User {
-                name : string
-                age : number
-                isAdmin : boolean
-                hasMI : Option<string>
-              }
-            `}
-          </SyntaxHighlighter>
-        </div>
-        <div style={{display: 'flex', flexDirection: 'column', width: '100%', alignItems: 'center'}}>
-          <h4>Server/api.tsx</h4>
-          <SyntaxHighlighter language='typescript'>
-            {`
-              function getUser(): Promise<Array<User>> {
-                return fetch(withRemoteBaseUrl(\\\`user\\\`))
-              }
-
-              function getUserByUserId(userId : number): Promise<User> {
-                return fetch(withRemoteBaseUrl(\\\`user/\${userId}\\\`))
-              }
-
-            `}
-          </SyntaxHighlighter>
-        </div>
+    <div>
+      <Header/>
+      <div className="level">
+        <Content />
       </div>
     </div>
   )
 }
+
+const Header = () => (
+    <section className="hero is-primary">
+        <div className="hero-body">
+            <div className="container">
+                <h1 className="title">
+                  Servant TS
+                </h1>
+            </div>
+        </div>
+    </section>
+)
+
+const Content = () => (
+    <div className="section" style={{display: 'flex', width: '100%'}}>
+        <div className="box" style={{display: 'flex', flexDirection: 'column'}}>
+            <h1>API</h1>
+            <SyntaxHighlighter language='haskell'>
+              {`
+              type SimpleAPI =
+                       "user" :> Get '[JSON] [User]
+                  :<|> "user" :> Capture "userId" Int :> Get '[JSON] User
+            `}
+            </SyntaxHighlighter>
+        </div>
+        <div className="box" style={{display: 'flex', flexDirection: 'column', width: '100%'}}>
+            <div className="box" style={{display: 'flex', flexDirection: 'column', width: '100%', alignItems: 'center'}}>
+                <h4>Server/types.tsx</h4>
+                <SyntaxHighlighter language='typescript'>
+                  {`
+                interface User {
+                  name : string
+                  age : number
+                  isAdmin : boolean
+                  hasMI : Option<string>
+                }
+              `}
+                </SyntaxHighlighter>
+            </div>
+            <div className="box" style={{display: 'flex', flexDirection: 'column', width: '100%', alignItems: 'center'}}>
+                <h4>Server/api.tsx</h4>
+                <SyntaxHighlighter language='typescript'>
+                  {`
+                function getUser(): Promise<Array<User>> {
+                  return fetch(withRemoteBaseUrl(\\\`user\\\`))
+                }
+
+                function getUserByUserId(userId : number): Promise<User> {
+                  return fetch(withRemoteBaseUrl(\\\`user/\${userId}\\\`))
+                }
+
+              `}
+                </SyntaxHighlighter>
+            </div>
+        </div>
+    </div>
+)
