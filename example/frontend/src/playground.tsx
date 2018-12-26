92f5329078abd2a7d10510fa6b7dda19ce1c13c8
Steven MacCoun <theraccoun@gmail.com>
2018-12-26 13:17:05 -0700

    Docs: split out playground views

--- example/frontend/src/playground.tsx
+++ example/frontend/src/playground.tsx
@@ -2,11 +2,10 @@ import * as React from 'react'
 import {Html} from "elm-ts/lib/React";
 import {Cmd, none} from "elm-ts/lib/Cmd";
 import { Dropdown, Button, Navbar, Box, Heading, Media, Columns, Level, Card } from "react-bulma-components/full";
-import {getApiLiteral, getTSFunctions, getTSTypes} from "./server/api";
-import {displayCodeFiled} from "./views/RenderedFiles";
-import { Lens, Optional } from 'monocle-ts'
+import { Lens } from 'monocle-ts'
+import {Content, Header} from "./views/PlaygroundViews";
 
-interface ViewState {
+export interface ViewState {
   isShowingOutput: boolean
   configuredFlavor: ConfiguredFlavor
 }
@@ -20,7 +19,7 @@ export type Model = ViewState
 
 export const init: [Model, Cmd<Msg>] = [{isShowingOutput: false, configuredFlavor: 'FpTs'}, none]
 
-enum MsgTypes {
+export enum MsgTypes {
   TOGGLE_SHOW_OUTPUT = 'TOGGLE_SHOW_OUTPUT',
   CHOOSE_FLAVOR = 'CHOOSE_FLAVOR'
 }
@@ -52,121 +51,5 @@ export function view(model: Model): Html<Msg> {
     </div>
   )
 }
-
-const Header = () => (
-  <Navbar color="info">
-      <Navbar.Brand>
-        <Navbar.Item>
-          <h1 className="title">
-            Servant TS
-          </h1>
-        </Navbar.Item>
-      </Navbar.Brand>
-      <Navbar.Menu>
-        <Navbar.Container position="end">
-            <Navbar.Item>
-              <a className="fab fa-github fa-2x" href="https://github.com/smaccoun/servant-ts">
-                <i ></i>
-              </a>
-            </Navbar.Item>
-        </Navbar.Container>
-      </Navbar.Menu>
-  </Navbar>
-)
-
-function Content(viewState: ViewState): Html<Msg> {
-  return dispatch => (
-    <Columns className="is-vcentered">
-      <Columns.Column>
-        {InputColumn(viewState)(dispatch)}
-      </Columns.Column>
-      {viewState.isShowingOutput ?
-        (<Columns className="is-vcentered">
-          <Columns.Column>
-            <i className="fas fa-arrow-right fa-4x"></i>
-          </Columns.Column>
-          <Columns.Column>
-            {viewServantTSOutputBox(viewState.configuredFlavor)}
-          </Columns.Column>
-        </Columns>
-        )
-          :
-          <div></div>
-        }
-
-
-    </Columns>
-  )
-}
-
-function InputColumn(model: Model): Html<Msg> {
-  return dispatch => (
-    <Box>
-      <Heading>Input</Heading>
-      {APIBox(model)(dispatch)}
-    </Box>
-    )
-}
-
-function APIBox(model: Model): Html<Msg> {
-  return dispatch => (
-    <div>
-      <Level>
-        <Level.Item>{displayCodeFiled("API.hs", getApiLiteral(), "haskell") }</Level.Item>
-      </Level>
-      <Level>
-        <Level.Side align="left"></Level.Side>
-        <Level.Side align="right">
-          <Level.Item>
-            {viewFlavorMenu(model.configuredFlavor)(dispatch)}
-          </Level.Item>
-          <Level.Item>
-            <Button
-              color="danger"
-              onClick={() => dispatch({type: MsgTypes.TOGGLE_SHOW_OUTPUT})}>
-              Run
-            </Button>
-          </Level.Item>
-        </Level.Side>
-      </Level>
-    </div>
-  )
-}
-
-function viewFlavorMenu(flavor: string): Html<Msg> {
-  return dispatch => (
-    <Dropdown value={flavor} onChange={(flavor) => dispatch({type: MsgTypes.CHOOSE_FLAVOR, flavor })}>
-      <Dropdown.Item value="FpTs">
-        FpTs
-      </Dropdown.Item>
-      <Dropdown.Item value="Vanilla">
-        Vanilla
-      </Dropdown.Item>
-    </Dropdown>
-  )
-}
-
-const viewServantTSOutputBox = (flavor: string) => (
-  <div className="box">
-    <Level>
-      <Heading>Output</Heading>
-    </Level>
-    <Level style={{marginTop: "10px"}}>
-    {displayCodeFiled(
-      "Server/types.tsx",
-      getTSTypes(flavor),
-      "typescript"
-    )}
-    </Level>
-    <Level>
-      {displayCodeFiled(
-        "Server/api.tsx",
-        getTSFunctions(),
-        "typescript"
-        )}
-    </Level>
-    </div>
-)
-
 
 
---
+++ example/frontend/src/views/PlaygroundViews.tsx
@@ -0,0 +1,123 @@
+import {Html} from "elm-ts/lib/React";
+import {displayCodeFiled} from "./RenderedFiles";
+import {getApiLiteral, getTSFunctions, getTSTypes} from "../server/api";
+import { Dropdown, Button, Navbar, Box, Heading, Media, Columns, Level, Card } from "react-bulma-components/full";
+import * as React from "react";
+import {Model, Msg, MsgTypes, ViewState} from "../playground";
+
+export const Header = () => (
+  <Navbar color="info">
+    <Navbar.Brand>
+      <Navbar.Item>
+        <h1 className="title">
+          Servant TS
+        </h1>
+      </Navbar.Item>
+    </Navbar.Brand>
+    <Navbar.Menu>
+      <Navbar.Container position="end">
+        <Navbar.Item>
+          <a className="fab fa-github fa-2x" href="https://github.com/smaccoun/servant-ts">
+            <i ></i>
+          </a>
+        </Navbar.Item>
+      </Navbar.Container>
+    </Navbar.Menu>
+  </Navbar>
+)
+
+export function Content(viewState: ViewState): Html<Msg> {
+  return dispatch => (
+    <Columns className="is-vcentered">
+      <Columns.Column>
+        {InputColumn(viewState)(dispatch)}
+      </Columns.Column>
+      {viewState.isShowingOutput ?
+        (<Columns className="is-vcentered">
+            <Columns.Column>
+              <i className="fas fa-arrow-right fa-4x"></i>
+            </Columns.Column>
+            <Columns.Column>
+              {viewServantTSOutputBox(viewState.configuredFlavor)}
+            </Columns.Column>
+          </Columns>
+        )
+        :
+        <div></div>
+      }
+
+
+    </Columns>
+  )
+}
+
+export function InputColumn(model: Model): Html<Msg> {
+  return dispatch => (
+    <Box>
+      <Heading>Input</Heading>
+      {APIBox(model)(dispatch)}
+    </Box>
+  )
+}
+
+function APIBox(model: Model): Html<Msg> {
+  return dispatch => (
+    <div>
+      <Level>
+        <Level.Item>{displayCodeFiled("API.hs", getApiLiteral(), "haskell") }</Level.Item>
+      </Level>
+      <Level>
+        <Level.Side align="left"></Level.Side>
+        <Level.Side align="right">
+          <Level.Item>
+            {viewFlavorMenu(model.configuredFlavor)(dispatch)}
+          </Level.Item>
+          <Level.Item>
+            <Button
+              color="danger"
+              onClick={() => dispatch({type: MsgTypes.TOGGLE_SHOW_OUTPUT})}>
+              Run
+            </Button>
+          </Level.Item>
+        </Level.Side>
+      </Level>
+    </div>
+  )
+}
+
+export function viewFlavorMenu(flavor: string): Html<Msg> {
+  return dispatch => (
+    <Dropdown value={flavor} onChange={(flavor) => dispatch({type: MsgTypes.CHOOSE_FLAVOR, flavor })}>
+      <Dropdown.Item value="FpTs">
+        FpTs
+      </Dropdown.Item>
+      <Dropdown.Item value="Vanilla">
+        Vanilla
+      </Dropdown.Item>
+    </Dropdown>
+  )
+}
+
+export const viewServantTSOutputBox = (flavor: string) => (
+  <div className="box">
+    <Level>
+      <Heading>Output</Heading>
+    </Level>
+    <Level style={{marginTop: "10px"}}>
+      {displayCodeFiled(
+        "Server/types.tsx",
+        getTSTypes(flavor),
+        "typescript"
+      )}
+    </Level>
+    <Level>
+      {displayCodeFiled(
+        "Server/api.tsx",
+        getTSFunctions(),
+        "typescript"
+      )}
+    </Level>
+  </div>
+)
+
+
